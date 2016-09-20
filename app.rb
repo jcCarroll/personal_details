d449c5014a9b63ca6b3902c413f7a18f2ce0720a
jacob <2staffi@gmail.com>
2016-09-20 13:39:18 -0400

    add cap to /age

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ get '/' do
 	erb :get_name
 end
 post '/the_users_name' do
-	name=params[:user_name]
-	"Hello #{name}"
+	name = params[:user_name]
+	redirect '/age?users_name=' + name
+end
+
+get '/age' do 
+	name = params[:users_name].capitalize
+		"Well hello #{name}"
 end

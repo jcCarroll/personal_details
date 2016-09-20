24aa6198fc613ca8c0e9b16b4ab70dc456269eb4
jacob <2staffi@gmail.com>
2016-09-20 14:10:45 -0400

    age

--- app.rb
+++ app.rb
@@ -5,10 +5,11 @@ get '/' do
 end
 post '/the_users_name' do
 	name = params[:user_name]
+	# redirect -- First part (/age) points to the next function. The ? passes in a new variable of (users_name).
 	redirect '/age?users_name=' + name
 end
 
 get '/age' do 
 	name = params[:users_name].capitalize
-		"Well hello #{name}"
+	erb :get_age, :locals => {:name => name}
 end
